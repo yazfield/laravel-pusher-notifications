c25ff40b7953b30c84eeb8631734f7e5efa32408
yazfield <hanifi.yazid@gmail.com>
2017-04-27 23:03:08 +0100

    Optimized isFollowing method

--- app/User.php
+++ app/User.php
@@ -58,6 +58,6 @@ class User extends Authenticatable
 
     public function isFollowing($userId)
     {
-        return (boolean) $this->follows()->where('follows_id', $userId)->count();
+        return (boolean) $this->follows()->where('follows_id', $userId)->first(['id']);
     }
 }
